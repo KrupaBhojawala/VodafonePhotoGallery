ef6811691c0c71ac3f43f777faca571e835fe0b6
KrupaBhojawala <bhojawalakrupa2000@gmail.com>
2020-04-29 14:56:20 +0530

    after build

---
+++ build/precache-manifest.75b049666bbcb71c0e8b23adfe750c2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45aaf8a50d8127a1a06a87e33c27e97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0879bd9087a4a94711a3",
+    "url": "/static/css/main.d1e9e308.chunk.css"
+  },
+  {
+    "revision": "2facdd4b4d2e1a36ca33",
+    "url": "/static/js/2.805a82aa.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.805a82aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0879bd9087a4a94711a3",
+    "url": "/static/js/main.f6d065c0.chunk.js"
+  },
+  {
+    "revision": "d8318b7374465af13d19",
+    "url": "/static/js/runtime-main.fd2ae6f7.js"
+  }
+]);
