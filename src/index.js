718c1f1b14152ff942f61cb48c8079e66f833bb1
KrupaBhojawala <bhojawalakrupa2000@gmail.com>
2020-04-30 17:01:42 +0530

    adding sevices

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './css/site.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { configureFakeBackend } from './FackBackend/fake-backend';
+// import { configureFakeBackend } from './FackBackend/fake-backend';
 
-configureFakeBackend();
+// configureFakeBackend();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
